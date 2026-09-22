9ef299d5e1040187be19ec5ee0c146251bea3f51
minhhyiii-dot <5bw2gthm74@privaterelay.appleid.com>
2026-09-23 01:03:07 +0700

    day3 update

---
+++ tracking/Day 3/Exercise1.py
@@ -0,0 +1,17 @@
+#Write the Python code that would print the type of each variable:
+
+#age = 20
+#height = 170.5
+#name = "Minhyi"
+#is_student = True
+
+#Your output code should contain 4 print() statements.
+
+age = 20
+height = 170.5
+name = "Minhyi"
+is_student = True
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
---
+++ tracking/Day 3/Exercise2.py
@@ -0,0 +1,31 @@
+#Create these four variables:
+
+#username → "Minhyi"
+#login_count → 3
+#account_balance → 1250.75
+#is_active → True
+
+#Then:
+
+#Reassign login_count to 4.
+#Reassign is_active to False.
+#Print the final value of all four variables.
+#Print the type of all four variables using type().
+
+username = "Minhyi"
+login_count = 3
+account_balance = 1250.75
+is_active = True
+
+login_count = 4
+is_active = False
+
+print(username)
+print(login_count)
+print(account_balance)
+print(is_active)
+
+print(type(username))
+print(type(login_count))
+print(type(account_balance))
+print(type(is_active))
